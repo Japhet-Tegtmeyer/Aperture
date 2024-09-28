70e246f8a35928bc6434d65bc5e72729c348e895
Japhet Tegtmeyer <japhteg@gmail.com>
2024-09-28 11:17:26 -0400

    Initial Commit

---
+++ Aperture/ApertureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApertureApp.swift
+//  Aperture
+//
+//  Created by Japhet Tegtmeyer on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ApertureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApertureTests/ApertureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApertureTests.swift
+//  ApertureTests
+//
+//  Created by Japhet Tegtmeyer on 9/28/24.
+//
+
+import Testing
+@testable import Aperture
+
+struct ApertureTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
